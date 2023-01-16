763579f2b19a4d05cdee7c8fdea4eae6cc3bf661
倚窗听雨丿醉无心 <994039348@qq.com>
2023-01-16 14:35:47 +0800

    添加样式文件

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createSSRApp } from 'vue';
 import App from './App.vue';
 
 import 'uno.css';
+import '@/styles/styles.scss';
 
 export function createApp() {
   const app = createSSRApp(App);
