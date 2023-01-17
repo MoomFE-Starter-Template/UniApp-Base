8c97020a7648cdd95eabebd6c899247171a24cf6
倚窗听雨丿醉无心 <994039348@qq.com>
2023-01-17 15:37:01 +0800

    安装 pinia 状态管理

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import pinia from '@/modules/pinia/index';
 
 import 'uno.css';
 import '@/styles/styles.scss';
 
 export function createApp() {
   const app = createSSRApp(App);
+
+  app.use(pinia);
+
   return {
     app,
   };
