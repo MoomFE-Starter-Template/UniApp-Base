bdcc03e958a222ac69462e3b8df8fac44224f997
倚窗听雨丿醉无心 <994039348@qq.com>
2023-01-18 10:41:01 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
+<script lang="ts" setup>
   onLaunch(() => {
-    console.log('App Launch');
+    console.log('App Launch'); // eslint-disable-line no-console
   });
   onShow(() => {
-    console.log('App Show');
+    console.log('App Show'); // eslint-disable-line no-console
   });
   onHide(() => {
-    console.log('App Hide');
+    console.log('App Hide'); // eslint-disable-line no-console
   });
 </script>
 
-<style></style>
+<style lang="scss">
+
+</style>
