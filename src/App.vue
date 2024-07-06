4dd8eee315738766e86218ef8b08a43c60be8895
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-06 14:36:28 +0800

    添加小程序版本更新提示

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <script lang="ts" setup>
+  // #ifdef MP
+  const updateManager = uni.getUpdateManager();
+
+  updateManager.onUpdateReady(() => {
+    uni.showModal({
+      title: '更新提示',
+      content: '新版本已经准备好，是否重启应用？',
+      success: (res) => {
+        if (res.confirm)
+          updateManager.applyUpdate();
+      },
+    });
+  });
+  // #endif
+
   onLaunch(() => {
     console.log('App Launch'); // eslint-disable-line no-console
   });
