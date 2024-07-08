31532b8a94b1f1325312442bcb0a23716bd07b7e
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-08 11:04:24 +0800

    优化账号密码登录逻辑

---
+++ src/pages/login/children/username-form.vue
@@ -0,0 +1,52 @@
+<template>
+  <view rounded-full bg-neutral-1>
+    <wd-input v-model="username" type="text" placeholder="请输入账号" clearable no-border />
+  </view>
+  <view rounded-full bg-neutral-1 mt-3>
+    <wd-input v-model="password" placeholder="请输入密码" show-password clearable no-border />
+  </view>
+</template>
+
+<script lang="ts" setup>
+  const username = ref('');
+  const password = ref('');
+
+  function validate() {
+    if (!username.value || !password.value) {
+      uni.showToast({ title: '请输入账号和密码', icon: 'none' });
+      return false;
+    }
+    return true;
+  }
+
+  function login() {
+
+  }
+
+  defineExpose({
+    validate,
+    login,
+  });
+</script>
+
+<script lang="ts">
+  export default defineComponent({
+    options: {
+      virtualHost: true,
+      styleIsolation: 'shared',
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  :deep(.wd-input){
+    --wot-input-bg: transparent;
+    --wot-input-inner-height-no-border: 43px;
+
+    --uno: pr-4;
+
+    input{
+      --uno: pl-4;
+    }
+  }
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -10,15 +10,68 @@
     <view flex="grow ~ col justify-center items-center">
       <TheLogo />
 
-      <view class="w-full mt-6">
-        <UsernameLogin v-if="type === 'username'" />
+      <view w-full mt-6 px-10>
+        <UsernameForm v-if="type === 'username'" ref="formRef" />
+
+        <view flex="~ items-center wrap" text="sm center neutral" mt-4>
+          <wd-checkbox v-model="agree" />
+          <text>已阅读并同意</text>
+          <text c-blue-6 @click="no()">《用户协议》</text>
+          <text>和</text>
+          <text c-blue-6 @click="no()">《隐私协议》</text>
+        </view>
+
+        <view mt-8>
+          <wd-button block @click="login()">登录</wd-button>
+        </view>
       </view>
+
+      <wd-message-box>
+        <text>请阅读并同意</text>
+        <text c-blue-6 @click="no()">《用户协议》</text>
+        <text>和</text>
+        <text c-blue-6 @click="no()">《隐私协议》</text>
+      </wd-message-box>
     </view>
   </view>
 </template>
 
 <script lang="ts" setup>
-  import UsernameLogin from './children/username-login.vue';
+  import { useMessage } from 'wot-design-uni';
+  import UsernameForm from './children/username-form.vue';
+
+  const message = useMessage();
 
   const type = ref<'username' | 'captcha'>('username');
+
+  const formRef = ref<InstanceType<typeof UsernameForm>>();
+
+  const agree = ref(false);
+
+  async function login() {
+    if (!await formRef.value?.validate()) return;
+    if (!await checkAgree()) return;
+
+    try {
+      await formRef.value?.login();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function checkAgree() {
+    if (agree.value) return true;
+
+    return message.confirm({
+      title: '服务协议与隐私保护',
+      confirmButtonText: '同意',
+      cancelButtonText: '不同意',
+    })
+      .then(() => agree.value = true);
+  }
+
+  function no() {
+    uni.showToast({ title: '暂无', icon: 'none' });
+  }
 </script>
