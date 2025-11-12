38a8a20a6a03771b94b69b56a9733ba7a70c09e4
倚窗听雨丿醉无心 <994039348@qq.com>
2025-11-12 14:20:59 +0800

    降级 unocss; 升级 `@dcloudio/*`

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,38 +3,34 @@ import { dirname, resolve } from 'node:path';
 import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import { dataToEsm } from '@rollup/pluginutils';
-import { outputFileSync } from 'fs-extra';
-import { defineConfig, presetAttributify, presetIcons, presetWind4, transformerDirectives, transformerVariantGroup } from 'unocss';
+import fs from 'fs-extra';
+import { isString } from 'mixte';
+import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss';
 import { presetApplet, presetRemRpx, transformerAttributify } from 'unocss-applet';
 import { presetExtra } from 'unocss-preset-extra';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const isApplet = process.env?.UNI_PLATFORM?.startsWith('mp-') ?? false;
+const isApplet = process.env?.UNI_PLATFORM?.startsWith('mp') ?? false;
 
-const presets: Preset[] = [
-  // 将 rpx 单位转为 rem
-  presetRemRpx({ mode: 'rpx2rem' }),
-];
+const presets: Preset[] = [];
 const transformers: SourceCodeTransformer[] = [];
 
 if (isApplet) {
   // 默认预设, 和 Tailwind 类似
-  presets.push(presetApplet({
-    preset: 'wind4',
-    presetOptions: { variablePrefix: 'un:' },
-  }));
+  presets.push(presetApplet({ variablePrefix: 'un:' }));
+  // 将 rpx 单位转为 rem
+  presets.push(presetRemRpx({ mode: 'rpx2rem' }));
   // 为小程序启用属性模式
-  transformers.push(transformerAttributify({
-    prefix: 'un:',
-    ignoreAttributes: ['block'],
-  }));
+  transformers.push(transformerAttributify({ prefix: 'un:', ignoreAttributes: ['block'] }));
 }
 else {
-  // 默认预设, 和 Tailwind 4 类似
-  presets.push(presetWind4());
+  // 默认预设, 和 Tailwind 类似
+  presets.push(presetUno());
   // 属性模式
   presets.push(presetAttributify({ prefix: 'un:' }));
+  // 将 rpx 单位转为 rem
+  presets.push(presetRemRpx({ mode: 'rpx2rem' }));
 }
 
 export default defineConfig({
@@ -62,7 +58,7 @@ export default defineConfig({
   ],
   extendTheme: (theme) => {
     // 始终生成一个 UnoCSS 主题样式配置文件, 方便在 JS 中引用
-    outputFileSync(
+    fs.outputFileSync(
       resolve(__dirname, './src/shared/unocss.theme.ts'),
       `/* eslint-disable */\n\n${dataToEsm(theme, {
         preferConst: true,
@@ -70,5 +66,12 @@ export default defineConfig({
         objectShorthand: true,
       })}`,
     );
+  },
+  postprocess: (util) => {
+    util.entries.forEach((i) => {
+      if (isString(i[1])) {
+        i[1] = i[1].replace(/in (oklch|oklab)/g, '');
+      }
+    });
   },
 });
